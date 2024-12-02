26446add5f0ecfabc05e15362f3f355099921ddb
p-moon <moon@jdk.plus>
2024-12-02 17:34:51 +0800

    完善单侧

---
+++ src/test/java/plus/jdk/smart/ioc/service/DegradeByGlobalConfigTest.java
@@ -0,0 +1,14 @@
+package plus.jdk.smart.ioc.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import plus.jdk.smart.ioc.TomcatLauncher;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TomcatLauncher.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class DegradeByGlobalConfigTest {
+
+}
--- src/test/java/plus/jdk/smart/ioc/service/SmartFlowByMethodArgsTest.java
+++ src/test/java/plus/jdk/smart/ioc/service/SmartFlowByMethodArgsTest.java
@@ -1,6 +1,10 @@
 package plus.jdk.smart.ioc.service;
 
 import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import plus.jdk.smart.ioc.TomcatLauncher;
 import plus.jdk.smart.ioc.global.Advised;
 import plus.jdk.smart.ioc.model.DispatchContext;
 import plus.jdk.smart.ioc.service.small.flow.SmsDispatchService;
@@ -9,17 +13,13 @@ import plus.jdk.smart.ioc.service.small.flow.impl.JdCloudSmsDispatchService;
 import plus.jdk.smart.ioc.service.small.flow.impl.TencentCloudSmsDispatchService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import plus.jdk.smart.ioc.TomcatLauncher;
 
 import javax.annotation.Resource;
 
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TomcatLauncher.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class SmartIocTest {
+public class SmartFlowByMethodArgsTest {
 
 
     /**
@@ -48,26 +48,33 @@ public class SmartIocTest {
 
     @Test
     public void testSmallFlow() throws InterruptedException {
+
+        // The input name parameter is ali, and the Alibaba Cloud interface is called to send text messages.
         DispatchContext context = new DispatchContext() {};
         context.setName("ali");
         Assert.assertTrue(smsDispatchService.dispatchMessage(context));
         Assert.assertTrue(context.getReceipt().startsWith("ali"));
 
+        // The input name parameter is tencent, and the interface of Tencent Cloud is called to send text messages.
         context.setName("tencent");
         Assert.assertTrue(smsDispatchService.dispatchMessage(context));
         Assert.assertTrue(context.getReceipt().startsWith("tencent"));
 
+        // The input name parameter is jd, and the interface of JD Cloud is called to send text messages.
         context.setName("jd");
         Assert.assertTrue(smsDispatchService.dispatchMessage(context));
         Assert.assertTrue(context.getReceipt().startsWith("jd"));
 
+        // The input name parameter is other, and the default main bean implementation is called to send text messages.
         context.setName("other");
         Assert.assertTrue(smsDispatchService.dispatchMessage(context));
         Assert.assertTrue(context.getReceipt().startsWith("default"));
 
+        // test the getOriginTarget method implemented by the proxy class
         Assert.assertTrue(smsDispatchService instanceof Advised);
         Assert.assertNotNull(((Advised) smsDispatchService).getOriginTarget());
 
+        // test the toString method implemented by the proxy class
         Assert.assertTrue(smsDispatchService.toString().contains("SDI"));
         log.info("hello");
     }
